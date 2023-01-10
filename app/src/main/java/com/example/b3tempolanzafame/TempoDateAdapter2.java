5934ab8a9a43239d9bdf2913f7dc43400190ba57
hugolanzafameynov <hugo.lanzafame@ynov.com>
2023-01-10 13:03:28 +0100

    finition (affichage du texte de la couleur dans histo v2)

--- app/src/main/java/com/example/b3tempolanzafame/TempoDateAdapter2.java
+++ app/src/main/java/com/example/b3tempolanzafame/TempoDateAdapter2.java
@@ -9,12 +9,12 @@ import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.b3tempoLanzafame.databinding.TempoDateItemBinding;
+import com.example.b3tempoLanzafame.databinding.TempoDateItem2Binding;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class TempoDateAdapter2 extends RecyclerView.Adapter<TempoDateAdapter2.TempoDateViewHolder> {
+public class TempoDateAdapter2 extends RecyclerView.Adapter<TempoDateAdapter2.TempoDateViewHolder2> {
 
     private List<TempoDate> tempoDates = new ArrayList<>();
     private Context context;
@@ -26,18 +26,18 @@ public class TempoDateAdapter2 extends RecyclerView.Adapter<TempoDateAdapter2.Te
 
     @NonNull
     @Override
-    public TempoDateViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public TempoDateViewHolder2 onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.tempo_date_item2, parent, false);
-        TempoDateItemBinding binding = TempoDateItemBinding.bind(view);
-        return new TempoDateViewHolder(binding);
+        TempoDateItem2Binding binding = TempoDateItem2Binding.bind(view);
+        return new TempoDateViewHolder2(binding);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull TempoDateViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull TempoDateViewHolder2 holder, int position) {
         holder.binding.dateTv.setText(tempoDates.get(position).getDate());
         holder.binding.colorFl.setBackgroundColor(ContextCompat.getColor(context, tempoDates.get(position).getCouleur().getColorResId()));
-        //holder.binding.colorText.setText(context.getString(tempoDates.get(position).getCouleur().getStringResId()));
+        holder.binding.colorText.setText(context.getString(tempoDates.get(position).getCouleur().getStringResId()));
     }
 
     @Override
@@ -45,10 +45,10 @@ public class TempoDateAdapter2 extends RecyclerView.Adapter<TempoDateAdapter2.Te
         return tempoDates.size();
     }
 
-    public class TempoDateViewHolder extends RecyclerView.ViewHolder {
-        TempoDateItemBinding binding;
+    public class TempoDateViewHolder2 extends RecyclerView.ViewHolder {
+        TempoDateItem2Binding binding;
 
-        public TempoDateViewHolder(@NonNull TempoDateItemBinding binding) {
+        public TempoDateViewHolder2(@NonNull TempoDateItem2Binding binding) {
             super(binding.getRoot());
             this.binding = binding;
         }
